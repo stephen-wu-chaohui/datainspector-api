af91214971cdcb71cc1267187342be0865193a45
stephen-wu-chaohui <stephen.wu.chaohui@gmail.com>
2020-02-22 09:52:54 +1300

    Add more tests

--- DataInspector.API/Controllers/DefaultController.cs
+++ DataInspector.API/Controllers/DefaultController.cs
@@ -2,20 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataInspector.API.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace DataInspector.API.Controllers
 {
-    [Route("api")]
+    [Route("api/default")]
     [ApiController]
     public class DefaultController : ControllerBase
     {
+        private IConfiguration configuration;
+
+        public DefaultController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // GET: api/Default
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Stephen's demo", "DataInspector API" };
+            return new string[] { "Stephen's demo",
+                "DataInspector API",
+                configuration.GetConnectionString("Default"),
+                configuration.GetValue<string>("AllowedOrigins")
+            };
         }
 
         // GET: api/Default/5
